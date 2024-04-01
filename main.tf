68a607204ba58ff550fd20f204df499880f13bed
Aryan Singh <58566951+aryandoescode@users.noreply.github.com>
2024-04-01 08:20:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,7 @@
 provider "azurerm" {
   features {}
 }
-resource "azurerm_subnet" "subnet1" {
-  name                 = "frontend"
-  resource_group_name  = "rg1"
-  virtual_network_name = "master-vnet"
-  address_prefixes     = ["10.0.3.0/24"]
-}
+
 resource "azurerm_public_ip" "vm_public_ip" {
   count                = 3
     name                 = "vm-public-ip-${count.index}"
@@ -71,14 +66,14 @@ resource "azurerm_network_interface" "vm_nic" {
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "default"
+    subnet_id                     = "/subscriptions/fa145963-b0f6-4b09-bbeb-076c3c14d4ab/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/master-vnet/subnets/default"
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.vm_public_ip[count.index].id
   }
   
 }
 resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.subnet1.id
+  subnet_id                 = "/subscriptions/fa145963-b0f6-4b09-bbeb-076c3c14d4ab/resourceGroups/rg1/providers/Microsoft.Network/virtualNetworks/master-vnet/subnets/default"
   network_security_group_id = azurerm_network_security_group.vm_nsg.id
 }
 
