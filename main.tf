bab6a3b97639ea06455d88a914fc3df11b62ac76
Aryan Singh <58566951+aryandoescode@users.noreply.github.com>
2024-04-01 07:47:02 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_public_ip" "vm_public_ip" {
+  count                = 3
+    name                 = "vm-public-ip-${count.index}"
+    location             = "East US"
+    resource_group_name  = "rg1"
+    allocation_method    = "Dynamic"
+          }
+resource "azurerm_virtual_machine" "linux_vm" {
+  count                 = 3
+  name                  = "linux-vm-${count.index}"
+  location              = "East US"
+  resource_group_name   = "rg1"
+  network_interface_ids = [azurerm_network_interface.vm_nic[count.index].id]
+  vm_size               = "Standard_DS2_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "osdisk-${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    computer_name  = "linux-vm-${count.index}"
+    admin_username = "aryan"
+    admin_password = "Aryan@6387402913"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = "East US"
+  resource_group_name = "rg1"
+
+  security_rule {
+    name                       = "AllowAll"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_network_interface" "vm_nic" {
+  count               = 3
+  name    = "vm-nic-${count.index}"
+  location            = "East US"
+  resource_group_name = "rg1"
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = "default"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip[count.index].id
+  }
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
